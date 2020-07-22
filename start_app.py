78e36408d16cbb195711aca3ca407a035e326bb3
hochlerer <hb@sem-wolf.org>
2020-07-22 03:58:26 +0300

    ordering the program

--- start_app.py
+++ start_app.py
@@ -1,8 +1,16 @@
 from complete import *
+from data_structure import data_initialization
 
-print("Enter your text: ")
-input = input()
+def start_App():
+    data_initialization()
+    while(1):
+        print("Enter your text: ")
+        input_ = input()
+        while input_ != "#":
+            print("Here are 5 suggestions")
+            complete(input_)
+            print(input_, end=" ")
+            input_ = input_ +" " + input()
 
-complete(input)
 
-# loop
+start_App()
