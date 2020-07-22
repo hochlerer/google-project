3dd0764ca062dd09257d0c2e3e8595fd15b7cd99
hochlerer <hb@sem-wolf.org>
2020-07-22 13:46:18 +0300

    Offline data initialization

--- start_app.py
+++ start_app.py
@@ -1,16 +1,19 @@
-from complete import *
 from data_structure import data_initialization
+from complete import *
+
+
 
 def start_App():
-    data_initialization()
     while(1):
         print("Enter your text: ")
         input_ = input()
-        while input_ != "#":
+        while input_[-1] != "#":
             print("Here are 5 suggestions")
-            complete(input_)
-            print(input_, end=" ")
-            input_ = input_ +" " + input()
+            suggestions = complete(input_)
+            for sent in suggestions:
+                print(sent.get_sentence())
+            print(input_, end="")
+            input_ = input_ + input()
 
 
 start_App()
